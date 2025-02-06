1528f3e98f1e64d3abda3262f428eae6383e4949
Yuliia <uliagrinenko1981@gmail.com>
2025-02-06 19:56:30 +0100

    implement Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(Factorial.factorialRec(5));
+        System.out.println(Fibonacci.fibonacci(0));
+        System.out.println(Fibonacci.fibonacci(5));
     }
 }
